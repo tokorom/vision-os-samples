69d948913d31db4366708cd478b010f262e965cc
tokorom <tokorom@gmail.com>
2023-06-26 17:44:09 +0900

    Add credit for licensing of 3D models.

--- 3DModelOnSwiftUISample/3DModelOnSwiftUISample/ContentView.swift
+++ 3DModelOnSwiftUISample/3DModelOnSwiftUISample/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // 3D Model by J CUBE Inc. - Maneki USDZ for AR / CC BY 4.0
+        // https://j-cube.jp/solutions/multiverse/assets/
+        // https://creativecommons.org/licenses/by-sa/4.0/
         Model3D(named: "maneki")
     }
 }
