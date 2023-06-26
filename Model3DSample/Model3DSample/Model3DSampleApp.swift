71de5dfa66e93c65f447dadee3fa5311b1c597e5
tokorom <tokorom@gmail.com>
2023-06-26 16:21:53 +0900

    Add Model3DSample

---
+++ Model3DSample/Model3DSample/Model3DSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Model3DSampleApp.swift
+//  Model3DSample
+//
+//  Created by Yuta Tokoro on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Model3DSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
