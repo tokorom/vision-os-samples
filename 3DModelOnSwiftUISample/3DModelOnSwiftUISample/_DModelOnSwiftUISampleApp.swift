a7dcff31b1d11f1451b4e906dd0d6ea032b597ac
tokorom <tokorom@gmail.com>
2023-06-26 16:25:25 +0900

    Add 3DModelOnSwiftUISample

---
+++ 3DModelOnSwiftUISample/3DModelOnSwiftUISample/_DModelOnSwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DModelOnSwiftUISampleApp.swift
+//  3DModelOnSwiftUISample
+//
+//  Created by Yuta Tokoro on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct _DModelOnSwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
