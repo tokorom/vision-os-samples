e4476ddaeef4520b02b6c4eb1d1b14f9ce9f3abc
tokorom <tokorom@gmail.com>
2023-06-28 12:28:04 +0900

    Add FullSpaceSample

---
+++ FullSpaceSample/FullSpaceSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FullSpaceSample
+//
+//  Created by Yuta Tokoro on 2023/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FullSpaceSample/FullSpaceSample/FullSpaceSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullSpaceSampleApp.swift
+//  FullSpaceSample
+//
+//  Created by Yuta Tokoro on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct FullSpaceSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
